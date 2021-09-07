b019f2dc2915fc50ec69a3b2d91d6f1bdc5ec887
Rafael Varela <rafaelvrcr@gmail.com>
2021-09-07 11:18:29 -0300

    Implementing the open popup and close popup button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Popup from "./components/Popup";
+import {useState} from 'react';
 
 function App() {
+  const [buttonPopup, setButtonPopup] = useState(false);
+  
   return (
     <div className="App">
       <main>
         <h1>Nutcache employees register tool</h1>
         <br/><br/>
-        <button>Register new employee</button>
-        <Popup trigger={false}>
+        <button onClick={() => setButtonPopup(true)}>Register new employee</button>
+        <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
           <h3>Register employee</h3>
+          <p>It's necessary to add fields with the data requsites</p>
         </Popup>
       </main>
     </div>
--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Popup.css'
 
 function Popup(props) {
     return (props.trigger) ? (
         <div className="popup">
             <div className="popup-inner">
-                <button className="close-btn">close</button>
+                <button className="close-btn" onClick={() =>props.setTrigger(false)}>close</button>
                 {props.children}
             </div>
         </div>
